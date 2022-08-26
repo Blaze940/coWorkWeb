9dee3bf59168e04c19ef2da64d469986ffe69d85
Mouss <mdiarra25@myges.fr>
2022-08-27 00:14:48 +0200

    unstable commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import { AppRoutingModule } from './app-routing.module';
 import {RouterModule} from "@angular/router";
-import { NotFoundComponent } from './not-found/not-found.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 import {MatIconModule} from "@angular/material/icon";
+import { HomeComponent } from './pages/home/home.component';
+import {FormsModule} from "@angular/forms";
+import { ListSpaceComponent } from './components/list-space/list-space.component';
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MaterialUiModule} from "./material-ui/material-ui.module";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
+    HomeComponent,
+    ListSpaceComponent,
   ],
   imports: [
     BrowserModule,
@@ -20,7 +28,11 @@ import {MatIconModule} from "@angular/material/icon";
     MatButtonModule,
     AppRoutingModule,
     RouterModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule,
+    MatCheckboxModule,
+    MaterialUiModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
